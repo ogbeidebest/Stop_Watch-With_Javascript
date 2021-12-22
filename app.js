a880e62d55b6c1815308f67270116b47e7374ca1
ogbeidebest <ogbeidebest.ob@gmail.com>
2021-12-22 15:05:58 +0100

    fixed the minute and the hour problem

--- app.js
+++ app.js
@@ -1,22 +1,79 @@
-let timer = 0;
-let timerInterval;
-let ms = document.getElementById('milliseconds');
-let second = document.getElementById('mainseconds');
-let minute = document.getElementById('mainminute');
 
-function start() {
-    stop();
-    timerInterval = setInterval(function(){
-        timer += 1/60;
-       msVal = Math.floor((timer - Math.floor (timer))*100);
-       secondVal = Math.floor(timer) - Math.floor (timer/60)*60;
-       minuteval = Math.floor (timer/60);
-       ms.innerHTML= msVal < 10? "0" + msVal.toString(): msVal;
-       second.innerHTML = secondVal < 10? "0" + secondVal.toString() : secondVal;
-       minute.innerHTML = minuteVal < 10? "0" + minuteVal.toString() : minuteVal; 
-    },100/60);
+let hours = 00;
+let minutes = 00;
+let seconds = 00;
+let milliseconds = 00;
+let interval;
+
+const hoursContainer = document.getElementById("hours");
+const minutesContainer = document.getElementById("minutes");
+const secondsContainer = document.getElementById("seconds"); const millisecondsContainer = document.getElementById("milliseconds");
+const start = document.getElementById("start");
+const stop = document.getElementById("stop");
+const reset = document.getElementById("reset");
+
+const startTimer = () => {
+    clearInterval(interval);
+    interval = setInterval(startWatch, 10);
 }
 
-function stop() {
-    clearInterval(timerInterval);
+const stopTimer = () => {
+    clearInterval(interval);
 }
+
+const resetTimer = () => {
+    milliseconds = 0;
+    seconds = 0;
+    minutes = 0;
+    hours = 0;
+
+    millisecondsContainer.innerHTML = "00";
+    secondsContainer.innerHTML = "00";
+    minutesContainer.innerHTML = "00";
+    hoursContainer.innerHTML = "00";
+}
+
+function startWatch() {
+    milliseconds++;
+    if (milliseconds < 9) {
+        millisecondsContainer.innerHTML = `0${milliseconds}`;
+    } else if (milliseconds > 99) {
+        milliseconds = 0;
+        millisecondsContainer.innerHTML = "00";
+        seconds++;
+    } else {
+        millisecondsContainer.innerHTML = milliseconds;
+    }
+
+    if (seconds < 10) {
+        secondsContainer.innerHTML = `0${seconds}`;
+    } else if (seconds > 59) {
+        minutes++;
+        seconds = 0;
+        secondsContainer.innerHTML = "00";
+    } else {
+        secondsContainer.innerHTML = seconds;
+    }
+
+    if (minutes < 10) {
+        minutesContainer.innerHTML = `0${minutes}`;
+    } else if (minutes > 59) {
+        hours++;
+        minutes = 0;
+        minutesContainer.innerHTML = minutes;
+    } else {
+        minutesContainer.innerHTML = minutes;
+    }
+
+    if (hours < 10) {
+        hoursContainer.innerHTML = `0${hours}`;
+    } else {
+        hoursContainer.innerHTML = hours;
+    }
+}
+
+
+
+start.addEventListener("click", startTimer);
+stop.addEventListener('click', stopTimer);
+reset.addEventListener("click", resetTimer);
